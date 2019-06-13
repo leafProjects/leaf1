edd237ef6da0cd223c9d50f1cbf8d772252df4f4
Pilone Matteo <pilone.matteo@gmail.com>
2019-06-13 23:07:29 +0200

    Configurazioni Iniziali SpringBoot

---
+++ src/main/java/com/projectleaf/leaf1/Leaf1Application.java
@@ -0,0 +1,13 @@
+package com.projectleaf.leaf1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Leaf1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Leaf1Application.class, args);
+	}
+
+}
