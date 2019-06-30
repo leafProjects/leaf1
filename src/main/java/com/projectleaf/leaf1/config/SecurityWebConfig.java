0f3fbbe9658489545b296c99f7f02ba6341dfe09
Pilone Matteo <pilone.matteo@gmail.com>
2019-06-30 17:47:29 +0200

    Auth Config INIT

--- src/main/java/com/projectleaf/leaf1/config/SecurityWebConfig.java
+++ src/main/java/com/projectleaf/leaf1/config/SecurityWebConfig.java
@@ -2,7 +2,6 @@ package com.projectleaf.leaf1.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
@@ -14,9 +13,10 @@ public class SecurityWebConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-			.antMatchers(HttpMethod.GET,"/**")
+			.antMatchers(HttpMethod.GET,"/signup")
 				.permitAll()
-			.antMatchers(HttpMethod.POST,"/**")
-				.permitAll();
+			.antMatchers(HttpMethod.POST,"/signup")
+				.permitAll()
+			.anyRequest().authenticated();
 	}
 }
